4391aaa2654aedebd217f5a52109fba4eadee98a
MukulSaini-007 <a2022it8622@imsec.ac.in>
2026-04-15 04:38:02 +0530

    fix build error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const [color, setColor] = useState("#000000");
   const [size, setSize] = useState(2);
-  const [strokes, setStrokes] = useState([]);
+  const [_strokes, setStrokes] = useState([]);
   const [cursors, setCursors] = useState({});
   const [activeDrawer, setActiveDrawer] = useState(null);
 
