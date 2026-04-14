dc10e54bb2c15dccea953cc2171650ba02773a74
MukulSaini-007 <a2022it8622@imsec.ac.in>
2026-04-15 04:14:58 +0530

    update frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://real-time-draw-backend.onrender.com");
 
 function App() {
   const canvasRef = useRef(null);
